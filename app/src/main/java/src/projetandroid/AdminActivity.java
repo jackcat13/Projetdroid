8dbe4ccd003b1afe5d27250734d22e8c340105ee
jackcat13 <christophehenry11@gmail.com>
2014-11-26 10:20:43 +0100

    maj panneau config

--- app/src/main/java/src/projetandroid/AdminActivity.java
+++ app/src/main/java/src/projetandroid/AdminActivity.java
@@ -1,9 +1,13 @@
 package src.projetandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class AdminActivity extends Activity {
@@ -13,9 +17,26 @@ public class AdminActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        Button gererPagesButton = (Button) findViewById(R.id.gererPagesButton);
+        Button gererIconesButton = (Button) findViewById(R.id.gererIconesButton);
 
+        gererPagesButton.setOnClickListener(gererPagesListener);
+        gererIconesButton.setOnClickListener(gererIconesListener);
     }
 
+    private OnClickListener gererIconesListener = new OnClickListener() {
+        public void onClick(View v) {
+            Intent intentGererPages = new Intent(AdminActivity.this, GererIconesActivity.class);
+            startActivity(intentGererPages);
+        }
+    };
+
+    private OnClickListener gererPagesListener = new OnClickListener() {
+        public void onClick(View v) {
+            Intent intentGererPages = new Intent(AdminActivity.this, GererPagesActivity.class);
+            startActivity(intentGererPages);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
