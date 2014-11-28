749eb07838c495be3012739bee6219b631211c17
caballoj <julien.caballol@hotmail.fr>
2014-11-28 09:54:37 +0100

    ViewIconesActivity terminé !

--- app/src/main/java/src/projetandroid/ViewIconesActivity.java
+++ app/src/main/java/src/projetandroid/ViewIconesActivity.java
@@ -1,39 +1,45 @@
 package src.projetandroid;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 import model.DBHelper;
 
 
-public class ViewIconesActivity extends Activity {
+public class ViewIconesActivity extends ListActivity {
     private DBHelper db;
-    private ArrayList<String> idPages;
+    private ListView list;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_icones);
 
+        db = new DBHelper(this);
+        list = (ListView) findViewById(R.id.iconesListView);
         ArrayList<String> listPages = new ArrayList<String>();
-        idPages = new ArrayList<String>();
 
         Cursor pages = db.selectPageQuery();
         pages.moveToFirst();
         while(!pages.isAfterLast()) {
-
-            ArrayList<String> page = new ArrayList<String>();
-            idPages.add( pages.getString(pages.getColumnIndex("IDPAGE")) );
             listPages.add( pages.getString(pages.getColumnIndex("NOMPAGE")) );
-
             pages.moveToNext();
         }
         pages.close();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listPages);
+        setListAdapter(adapter);
+
     }
 
 
@@ -50,6 +56,13 @@ public class ViewIconesActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Bundle b=new Bundle();
+
+        b.putInt("idIcone",id);
+
+        Intent intentAjouterIcone = new Intent(ViewIconesActivity.this, ModifierIconeActivity.class);
+        intentAjouterIcone.putExtras(b);
+        startActivity(intentAjouterIcone);
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
